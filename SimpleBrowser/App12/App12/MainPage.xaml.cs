6a57398c00631b27b992033ddcbf91e7e4c83328
Supun Sarachitha Liyanaarachchi <36870488+supunsarachitha@users.noreply.github.com>
2018-08-19 01:32:37 +0530

    Initial code
    xamarin.forms simple browser v1.0

---
+++ SimpleBrowser/App12/App12/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App12
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void SearchButton_Clicked(object sender, EventArgs e)
+        {
+            Browser.Source = "http://www.google.com/search?q=" + SearchBar.Text;
+            SearchButton.Text = "Loading...";
+            await Task.Delay(1000);
+        }
+
+        private void Browser_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            SearchButton.Text = "Back";
+
+        }
+    }
+}
